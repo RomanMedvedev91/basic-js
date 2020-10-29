570a771b8dcf5dd9dcc6ca1398cee879b4534aba
Медведев Роман Михайлович <roman.m.medvedev@ent-en.com>
2020-10-29 18:00:51 +0500

    init: intial commit

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(array) {
+  // throw new CustomError('Not implemented');
+  const arr = array.reduce((acc, val) => {
+    return acc.concat(val);
+  }, []);
+  // const count = 0;
+  const cats = arr.reduce((acc, val) => {
+    if (val === "^^") acc++;
+    return acc;
+  }, 0);
+  return cats;
   // remove line with error and write your code here
 };
