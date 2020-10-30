6a91b276432841032e4a28a6bc9cf8ca59606d8b
Медведев Роман Михайлович <roman.m.medvedev@ent-en.com>
2020-10-30 18:01:22 +0500

    feat: solve 1st task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,16 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError("Not implemented");
+module.exports = function dateSample(sampleActivity) {
+  if (sampleActivity > 0 && sampleActivity !== Infinity) {
+    // throw new CustomError("Not implemented");
+    const ka = 0.693 / HALF_LIFE_PERIOD;
+
+    const ages = Math.round(Math.log(MODERN_ACTIVITY / sampleActivity) / ka);
+    console.log(ages);
+    return ages;
+  } else {
+    return false;
+  }
   // remove line with error and write your code here
 };
